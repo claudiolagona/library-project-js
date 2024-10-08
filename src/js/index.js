a2b291f122490d63f02e11f4e96684903a73b8ef
Claudio La Gona <79311818+claudiolagona@users.noreply.github.com>
2024-10-08 12:30:34 +0200

    initial push

---
+++ src/js/index.js
@@ -0,0 +1,9 @@
+import "../css/style.scss";
+
+class Test {}
+
+const t1 = new Test();
+
+console.log("Hello World!");
+
+console.log(process.env.DB_HOST);
---
+++ webpack.config.js
@@ -0,0 +1,65 @@
+// Generated using webpack-cli https://github.com/webpack/webpack-cli
+
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+const isProduction = process.env.NODE_ENV == "production";
+
+const stylesHandler = "style-loader";
+
+const Dotenv = require("dotenv-webpack");
+
+const config = {
+  entry: "./src/js/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  devServer: {
+    open: true,
+    host: "localhost",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+
+    new Dotenv({
+      systemvars: true,
+    }),
+
+    // Add your plugins here
+    // Learn more about plugins from https://webpack.js.org/configuration/plugins/
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/i,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+      },
+      {
+        test: /\.(css|s[ac]ss)$/i,
+        use: [stylesHandler, "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.(eot|svg|ttf|woff|woff2|png|jpe?g|gif)$/i,
+        use: {
+          loader: "img-optimize-loader",
+        },
+      },
+
+      // Add your rules for custom modules here
+      // Learn more about loaders from https://webpack.js.org/loaders/
+    ],
+  },
+};
+
+module.exports = () => {
+  if (isProduction) {
+    config.mode = "production";
+  } else {
+    config.mode = "development";
+  }
+  return config;
+};
